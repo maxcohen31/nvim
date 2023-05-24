7302525f8b5e899e1954d132082ed47875f8ad59
maxcohen31 <eman_93@hotmail.it>
2023-05-24 12:38:31 +0200

    fix line 11-12 keympaps.lua

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -8,8 +8,8 @@ vim.opt.cursorline = true
 vim.opt.autoread = true
 
 -- use spaces for tabs and whatnot
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.shiftround = true
 vim.opt.expandtab = true
 
